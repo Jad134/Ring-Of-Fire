c835e708463d8f35bcb12814a15b75437f868344
Jad El Nader <Jadelnader99@gmail.com>
2024-01-09 15:40:33 +0100

    add Firebase connection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-592bf","appId":"1:475112071781:web:d3142680a19a10ab0d1c8c","storageBucket":"ring-of-fire-592bf.appspot.com","apiKey":"AIzaSyAq_3SoG-zjgnh_LO1YpyzzE7Xa30jNSMQ","authDomain":"ring-of-fire-592bf.firebaseapp.com","messagingSenderId":"475112071781"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,6 +8,10 @@ import { MatDialog, } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { FormsModule } from '@angular/forms';
 import { GameInfoComponent } from '../game-info/game-info.component';
+import { Firestore, collection, doc, collectionData, onSnapshot, addDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Console, log } from 'console';
+import { ActivatedRoute } from '@angular/router';
 
 
 
@@ -23,15 +27,81 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   game: Game = new Game();
   currentCard: string = '';
-  constructor(public dialog: MatDialog) { }
+  firestore: Firestore = inject(Firestore);
+  items$: any;
+  items: any;
+  unsubList: any;
+  unSubSingle:any;
+
+
+
+  constructor(private route: ActivatedRoute, public dialog: MatDialog) {
+
+    // this.unsubList = onSnapshot(this.getGameRef(), (list) => {
+    //   list.forEach(element => {
+    //     console.log(element.data());
+    //   })
+    // });
+
+    // this.unSubSingle = onSnapshot(this.getSingleDocRef("games", "ffeffe"), (element) => {
+
+    // })
+
+    // this.unSubSingle();
+    // this.items$ = collectionData(this.getGameRef());
+    // this.items = this.items$.subscribe((list:any) => {
+    //   list.forEach(element => {
+    //     console.log(element)
+    //   });
+    // })
+    // this.items.unsubsribe();
+  }
   ngOnInit(): void {
     this.newGame();
+    this.route.params.subscribe(params => {
+      const gameId = params['id'];
+      console.log(gameId);
+
+      // Verwenden Sie die ID, um Firestore-Abfragen durchzuführen
+      this.unSubSingle = onSnapshot(this.getSingleDocRef("games", gameId), (currentGame) => {
+        console.log('update',currentGame.data());
+        const gameData = currentGame.data()!;
+        this.game.currentPlayer = gameData['currentplayer'];
+        this.game.playedCards = gameData['playedCards'];
+        this.game.players = gameData['players'];
+        this.game.stack = gameData['stack'];
+      });
+    })
 
   }
 
-  newGame() {
+  ngonDestroy() {
+    if (this.unSubSingle) {
+      this.unSubSingle();
+    }
+  }
+
+  getGameRef() {
+    return collection(this.firestore, 'games')
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId)
+  }
+
+  async newGame() {
+
     this.game = new Game();
-    console.log(this.game);
+
+    //-----------------------------Aktivieren für das hochladen von neuen spielen------------------------
+    // await addDoc(this.getGameRef(),this.game.toJson()).catch(
+    //   (err) => {console.error(err)}
+    // ).then(
+    //   (docRef) => {console.log('Document written with ID:', docRef?.id);}
+    // )
+    // console.log(this.game);
+
+
   }
 
   takeCard() {
@@ -57,7 +127,7 @@ export class GameComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(name => {
-      if(name && name.length > 0) {
+      if (name && name.length > 0) {
         this.game.players.push(name);
       }
     });
